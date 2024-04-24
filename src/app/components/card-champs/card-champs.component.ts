13f828eea530a46290c72bd812ccd3226e52aed3
mdiasfran <contate.afran@gmail.com>
2024-04-24 00:56:26 -0300

    Cria card champs

--- src/app/components/card-champs/card-champs.component.ts
+++ src/app/components/card-champs/card-champs.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardChampsComponent implements OnInit {
 
+  championClasses: Array<any> = [
+    { id: 1, name: "Altruist" },
+    { id: 2, name: "Arcanist" },
+    { id: 3, name: "Artist" },
+    { id: 4, name: "Behemoth" },
+    { id: 5, name: "Bruiser" },
+    { id: 6, name: "Duelist" },
+    { id: 7, name: "Exalted" },
+    { id: 8, name: "Great" },
+    { id: 9, name: "Invoker" },
+    { id: 10, name: "Reaper" },
+    { id: 11, name: "Sage" },
+    { id: 12, name: "Sniper" },
+    { id: 13, name: "Spirit Walker" },
+    { id: 14, name: "Trickshot" },
+    { id: 15, name: "Warden" }
+  ]
   constructor() { }
 
   ngOnInit(): void {
